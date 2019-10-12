6e67950dd756547a9314d66172365fd331ac5ba7
RJLyders <richard@lyders.com>
2019-10-12 12:36:47 -0500

    completed:    Passing Data to a State Object

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -2,11 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 
 class LocationScreen extends StatefulWidget {
+  LocationScreen({this.locationWeather});
+
+  final locationWeather;
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+  int temperature;
+  int condition;
+  String cityName;
+
+  @override
+  void initState() {
+    super.initState();
+    updateUI(widget.locationWeather);
+  }
+
+  void updateUI(dynamic weatherData) {
+    double temp = weatherData['main']['temp'];
+    temperature = temp.toInt();
+    condition = weatherData['weather'][0]['id'];
+    cityName = weatherData['name'];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,7 +69,7 @@ class _LocationScreenState extends State<LocationScreen> {
                 child: Row(
                   children: <Widget>[
                     Text(
-                      '32°',
+                      '$temperature°',
                       style: kTempTextStyle,
                     ),
                     Text(
@@ -74,11 +94,3 @@ class _LocationScreenState extends State<LocationScreen> {
     );
   }
 }
-/*
-double temperature = decodedData['main']['temp'];
-int conditionId = decodedData['weather'][0]['id'];
-String cityName = decodedData['name']; // name
-double longitude = decodedData['coord']['lon'];
-double latitude = decodedData['coord']['lat'];
-String weatherDescription = decodedData['weather'][0]['description'];
-*/
